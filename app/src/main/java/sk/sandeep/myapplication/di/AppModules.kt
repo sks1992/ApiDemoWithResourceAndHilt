8206fdc8ded2801126368dfe377e00c3feb70629
Sandeep <sandeepsk15892>
2022-08-25 17:55:45 +0530

    demo app

--- app/src/main/java/sk/sandeep/myapplication/di/AppModules.kt
+++ app/src/main/java/sk/sandeep/myapplication/di/AppModules.kt
@@ -1,10 +1,31 @@
 package sk.sandeep.myapplication.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import sk.sandeep.myapplication.model.remote.NewsApi
+import sk.sandeep.myapplication.util.BASE_URL
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModules {
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsApi(retrofit: Retrofit): NewsApi {
+        return retrofit.create(NewsApi::class.java)
+    }
 }
--- app/src/main/java/sk/sandeep/myapplication/model/repository/NewsRepository.kt
+++ app/src/main/java/sk/sandeep/myapplication/model/repository/NewsRepository.kt
@@ -1,9 +1,15 @@
 package sk.sandeep.myapplication.model.repository
 
+import sk.sandeep.myapplication.model.data_models.NewsApiResponse
 import sk.sandeep.myapplication.model.remote.NewsApi
+import sk.sandeep.myapplication.util.Resource
 import javax.inject.Inject
 
 class NewsRepository @Inject constructor(
     private val newsApi: NewsApi
 ) {
+
+//    suspend fun getTopHeadlines(countryCode:String):Resource<NewsApiResponse>{
+//        return newsApi.getTopHeadlines(countryCode)
+//    }
 }
